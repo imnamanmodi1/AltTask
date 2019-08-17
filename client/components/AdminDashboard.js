66a7cdaf2891a2749666dc554d84cfd6debd7a8d
akslucky619 <akash619@protonmail.com>
2019-08-17 15:25:57 +0530

    add chartjs

--- client/components/AdminDashboard.js
+++ client/components/AdminDashboard.js
@@ -12,6 +12,15 @@ class AdminDashboard extends Component {
             label:"Tasks",
             data:[
               12,24,9,14
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
             ]
           }
         ]
@@ -21,7 +30,15 @@ class AdminDashboard extends Component {
   render() {
     return(
       <div className="chart">
-        <Bar data={this.state.chartData}/>
+        <Bar data={this.state.chartData}
+          options={{
+            title:{
+              display:true,
+              text: "Task History",
+              fontSize:25
+            }
+          }}
+        />
       </div>
     );
   }
