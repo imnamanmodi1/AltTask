fbb847db11e15e3c6f70bd4e34f2263194e623a9
akslucky619 <akash619@protonmail.com>
2019-08-17 15:13:40 +0530

    add chartjs

--- client/components/AdminDashboard.js
+++ client/components/AdminDashboard.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
+import {Bar, Line, Pie} from 'react-chartjs-2'
 
 class AdminDashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      chartData:{
+        labels: ['Week1','Week2','Week3','Week4'],
+        datasets:[
+          {
+            label:"Tasks",
+            data:[
+              12,24,9,14
+            ]
+          }
+        ]
+      }
+    };
   }
   render() {
-    return <>Hi this is admin dashboard</>;
+    return(
+      <div className="chart">
+        <Bar data={this.state.chartData}/>
+      </div>
+    );
   }
 }
 
